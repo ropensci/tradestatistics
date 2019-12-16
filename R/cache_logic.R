f959d73d67c2a8beb08efd350fcc3d99ad8b08a1
Unknown <eliocampitelli@gmail.com>
2019-12-16 18:24:10 -0300

    Adds proper cache (with hash!)

--- R/cache_logic.R
+++ R/cache_logic.R
@@ -1,16 +1,24 @@
 
-with_cache <- function(use_cache, file, memoised, unmemoised, 
-                       read_function = read.csv, 
-                       write_function = function(data, file) write.csv(data, file, row.names = FALSE), 
+with_cache <- function(use_cache, file, memoised, unmemoised,
                        ...) {
   # cache in memory
   if (use_cache && is.null(file)) {
     return(memoised(...))
   }
   
+  if (!is.null(file)) { 
+    hash <- digest::digest(list(body(unmemoised), ...))
+  } 
+  
   # cache in file  
   if (use_cache && file.exists(file)) {
-    return(read_function(file))
+    data <- data.table::fread(file, yaml = TRUE)
+    
+    if (data$.hash[1] == hash) {
+      class(data) <- c("tbl_df", "tbl", "data.frame")
+      data$.hash <- NULL
+      return(data)
+    }
   }
   
   if(!curl::has_internet()){
@@ -19,26 +27,16 @@ with_cache <- function(use_cache, file, memoised, unmemoised,
   }
   data <- unmemoised(...)
   
-  if (!is.null(file)) { 
-    write_function(data, file)
+  if (!is.null(file)) {
+    data$.hash <- hash
+    data.table::fwrite(data, file, yaml = TRUE)
+    data$.hash <- NULL
   }
   
   if (!use_cache) {
     memoise::forget(memoised)
   } 
   
-  return(data)
-}
-
-
-write_ots_data <- function(data, file) {
-  data.table::fwrite(data, file, yaml = TRUE)
-}
-
-read_ots_data <- function(file) {
-  data <- data.table::fread(file, yaml = TRUE)
-  attr(data, "yaml_metadata") <- NULL
-  class(data) <- c("tbl_df", "tbl", "data.frame")
   return(data)
 }
 
