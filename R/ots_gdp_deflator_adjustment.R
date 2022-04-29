24892a27c81429f6617698d09450767572f0f058
Mauricio Vargas <mavargas11@uc.cl>
2022-04-29 02:07:26 -0400

    API is back!

---
+++ R/ots_gdp_deflator_adjustment.R
@@ -0,0 +1,80 @@
+#' Expresses tidy data from the API in dollars of a reference year
+#' @description Uses GDP deflator records from The World Bank to
+#' convert trade records and express them in dollars of the same year. The
+#' records are internally subsetted to World's values, because country specific
+#' levels would largely re-scale observations for reporters that reflect
+#' unstable macroeconomic policies.
+#' @param trade_data A tibble obtained by using ots_create_tidy_data.
+#' Default set to \code{NULL}.
+#' @param reference_year Year contained within the years specified in
+#' api.tradestatistics.io/year_range (e.g. \code{2010}).
+#' Default set to \code{NULL}.
+#' @importFrom data.table `:=` rbindlist last
+#' @export
+#' @examples
+#' \dontrun{
+#' # The next example can take more than 5 seconds to compute,
+#' # so this is shown without evaluation according to CRAN rules
+#'
+#' # Convert dollars of 2010 to dollars of 2000
+#' d <- ots_create_tidy_data(years = 2010, reporters = "chl", partners = "chn")
+#' ots_gdp_deflator_adjustment(trade_data = d, reference_year = 2000)
+#' }
+#' @keywords functions
+ots_gdp_deflator_adjustment <- function(trade_data = NULL, reference_year = NULL) {
+  # Check input -------------------------------------------------------------
+  if (is.null(trade_data)) {
+    stop("The input data cannot be NULL.")
+  }
+
+  if (is.null(reference_year)) {
+    stop("The reference year cannot be NULL."
+    )
+  }
+
+  ots_gdp_deflator_min_year <- min(tradestatistics::ots_gdp_deflator$from)
+  ots_gdp_deflator_max_year <- max(tradestatistics::ots_gdp_deflator$from)
+
+  if (!is.numeric(reference_year) |
+    !(reference_year >= ots_gdp_deflator_min_year &
+      reference_year <= ots_gdp_deflator_max_year)) {
+    stop(sprintf("The reference year must be numeric and contained within ots_gdp_deflator years range that is %s-%s.",
+        ots_gdp_deflator_min_year,
+        ots_gdp_deflator_max_year
+      )
+    )
+  }
+
+  # Filter year conversion rates and join data ------------------------------
+  years <- unique(trade_data$year)
+  
+  d1 <- lapply(
+    years,
+    function(year) {
+      if (year < reference_year) {
+        tradestatistics::ots_gdp_deflator[to <= reference_year & to > year &
+          country_iso == "wld",
+          .(gdp_deflator = last(cumprod(gdp_deflator)))][,
+          `:=`(year = ..year, conversion_year = ..reference_year)][,
+          .(year, conversion_year, gdp_deflator)]
+      } else if (year > reference_year) {
+        tradestatistics::ots_gdp_deflator[from >= reference_year & from < year &
+          country_iso == "wld",
+          .(gdp_deflator = 1/last(cumprod(gdp_deflator)))][,
+          `:=`(year = ..year, conversion_year = ..reference_year)][,
+          .(year, conversion_year, gdp_deflator)]
+      } else if (year == reference_year) {
+        data.frame(
+          year = year, conversion_year = year, gdp_deflator = 1
+        )
+      }
+    }
+  )
+  d1 <- rbindlist(d1)
+
+  d2 <- trade_data[d1, on = .(year), allow.cartesian = TRUE][,
+    `:=`(trade_value_usd_exp = round(trade_value_usd_exp * gdp_deflator, 0),
+         trade_value_usd_imp = round(trade_value_usd_imp * gdp_deflator, 0))]
+
+  return(d2)
+}
--- R/tradestatistics-package.R
+++ R/tradestatistics-package.R
@@ -3,11 +3,160 @@
 
 utils::globalVariables(c(
   "year", "country_iso",
-  "country_fullname_english",
-  "commodity_fullname_english", "group_fullname_english", "commodity_code", 
+  "country_name_english", "country_fullname_english",
+  "commodity_fullname_english", "section_fullname_english", "commodity_code", 
   "group_code", "trade_value_usd_exp", "trade_value_usd_imp",
   "trade_value_usd_top_exp", "trade_value_usd_top_imp",
-  "conversion_factor", "conversion_year", "from", "to", "observation",
+  "gdp_deflator", "conversion_factor", "conversion_year", "from", "to", "observation",
   "..group", "..productname", "..section",
   "..reference_year", "..year", "..columns_order", "."
   ))
+
+#' OTS Tables
+#'
+#' A table describing existing API tables with both description and source.
+#' This data is used by the functions provided within this package to validate
+#' user parameters.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ots_tables
+#' @usage ots_tables
+#' @source Open Trade Statistics
+#' @format A data frame with 16 rows and 3 variables
+#' \itemize{
+#'   \item{\code{table}}{Table name}
+#'   \item{\code{description}}{Description of table contents}
+#'   \item{\code{source}}{Source for the data (OTS tables are processed after UN Comtrade raw data)}
+#' }
+NULL
+
+#' GDP Deflator
+#'
+#' A table with the World's weigthed GDP deflator inflation since 2000.
+#' Provides year to year GDP deflator values for every country listed in the 
+#' World Bank database. This data is ready to be applied as a conversion rate 
+#' to express dollars of year Y1 as dollars of year Y2.
+#' For countries not listed in the World Bank database, rows labelled as "wld" 
+#' are available, which were computed as the weighted median for each year using 
+#' the GDP of listed countries for each year expressed as constant dollars of 
+#' the year 2010.
+#' This dataset is provided to be used with \code{ots_gdp_deflator_adjustment} 
+#' that converts units forwards and backwards in time.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ots_gdp_deflator
+#' @usage ots_gdp_deflator
+#' @source Open Trade Statistics
+#' @format A data frame with 7,238 observations on the following 4 variables
+#' \itemize{
+#'   \item{\code{country_iso}}{ISO code of the country (e.g. "chl" means Chile)}
+#'   \item{\code{from}}{Integer values in the range 1980-2018}
+#'   \item{\code{to}}{Integer values in the range 1981-2019}
+#'   \item{\code{gdp_deflator}}{Numeric value expressed as one plus 1-year deflator}
+#' }
+NULL
+
+#' OTS Countries
+#'
+#' A table of official country names, ISO-3 codes and other metadata. The
+#' availability is limited to countries with records in the OTS database.
+#' This data is used by the functions provided within this package to complement
+#' the data obtained from the API.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ots_countries
+#' @usage ots_countries
+#' @source Open Trade Statistics
+#' @format A data frame with 264 observations on the following 6 variables
+#' \itemize{
+#'   \item{\code{country_iso}}{ISO code of the country (e.g. "chl" means Chile)}
+#'   \item{\code{country_name_english}}{Country name (e.g. Germany)}
+#'   \item{\code{country_fullname_english}}{Country name with indications (e.g. Germany (former Federal Republic of Germany until 1990))}
+#'   \item{\code{continent_id}}{Numeric id of the continent where the country belongs to}
+#'   \item{\code{continent_name_english}}{Continent where the country belongs to}
+#'   \item{\code{eu28_member}}{Dummy variable such that 1 means "belongs to EU-28 group" and 0 otherwise}
+#' }
+NULL
+
+#' OTS Commodities
+#'
+#' A table of official commodity names from the Harmonized System rev 2012
+#' (HS12, also known as H4). Provides official commodity and section codes and 
+#' names at six (6) digits detail taken from the United Nations official 
+#' sources.
+#' This data is used by the functions provided within this package to complement
+#' the data obtained from the API.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ots_commodities
+#' @usage ots_commodities
+#' @source Open Trade Statistics
+#' @format A data frame with 5,304 observations on the following 4 variables
+#' \itemize{
+#'   \item{\code{commodity_code}}{Code of every commodity (e.g. 010110)}
+#'   \item{\code{commodity_fullname_english}}{HS commodity names (e.g. 'Horses, asses, mules and hinnies; live, pure-bred breeding animals')}
+#'   \item{\code{section_code}}{Section code (e.g. 01)}
+#'   \item{\code{section_fullname_english }}{Section name (e.g. 'Live animals and animal products')}
+#' }
+NULL
+
+#' OTS Commodities Short
+#'
+#' A table of official commodity names from the Harmonized System rev 2012
+#' (HS12, also known as H4). Provides official commodity and section codes and 
+#' names at four (4) digits of detail taken from the United Nations official 
+#' sources.
+#' This data is used by the functions provided within this package to complement
+#' the data obtained from the API.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ots_commodities_short
+#' @usage ots_commodities_short
+#' @source Open Trade Statistics
+#' @format A data frame with 1,225 observations on the following 2 variables
+#' \itemize{
+#'   \item{\code{commodity_code}}{Code of every commodity (e.g. 010110)}
+#'   \item{\code{commodity_fullname_english}}{HS commodity names (e.g. 'Horses, asses, mules and hinnies; live, pure-bred breeding animals')}
+#'   \item{\code{section_code}}{Section code (e.g. 01)}
+#'   \item{\code{section_fullname_english }}{Section name (e.g. 'Live animals and animal products')}
+#' }
+NULL
+
+#' OTS Sections
+#'
+#' A table of official section names from the Harmonized System rev 2012 
+#' (HS12, also known as H4).
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ots_sections
+#' @usage ots_sections
+#' @source Adapted from UN COMTRADE
+#' @format A data frame with 22 rows and 2 variables
+#' \itemize{
+#'   \item{\code{section_code}}{Section code (e.g. '01')}
+#'   \item{\code{section_fullname_english}}{Section hex color (e.g. 'Live animals and animal products')}
+#' }
+NULL
+
+#' OTS Sections Colors
+#'
+#' A table of official unofficial colors to ease visualization for the sections
+#' the Harmonized System rev 2012 (HS12, also known as H4).
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ots_sections_colors
+#' @usage ots_sections_colors
+#' @source Open Trade Statistics
+#' @format A data frame with 22 rows and 2 variables
+#' \itemize{
+#'   \item{\code{section_code}}{Section code (e.g. '01')}
+#'   \item{\code{section_color}}{Section hex color (e.g. '#74c0e2')}
+#' }
+NULL
